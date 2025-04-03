c2c044e0c173193cf3075d5f721afc232e0f72c4
Steven Phung <sphung01@bu.edu>
2025-04-02 22:58:57 -0400

    Added features in the transtion component

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -1,2 +1,20 @@
 "use client"
 
+import { motion } from "framer-motion";
+
+export default function Transition({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ ease: 'easeIn', duration: 0.75}}
+    >
+        {children}
+    </motion.div>
+  );
+}
+
